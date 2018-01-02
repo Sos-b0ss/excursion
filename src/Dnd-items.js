6a15e394ac820dc2b29f869edd341be1aeaac8b2
Sos_b0ss <gabrielrodd@gmail.com>
2018-01-02 13:12:47 -0700

    First commit for Dnd randomizer project

---
+++ src/Dnd-items.js
@@ -0,0 +1,23 @@
+let listOne = ['hammer','waraxe','splitting-axe','morningstar','sling (6 led balls)'];
+let listTwo = ['commoners clothing','shortsword','dagger','iron helmet','torch'];
+let listThree = ['warhammer','bo-staff','5 x darts','rapier','club','wine','potion of healing'];
+let listFour = ['helmet of seeking','dagger','torch','wine','crossbow w/ 15 bolts'];
+let listFive = ['wine','potion of healing','scroll of undead','net','whip'];
+let listSix = ['scimitar','halberd','shortsword','kiteshield','flail','rapier','javalin'];
+let listSeven = ['crowbar','torch','scimitar','kitchenknife','club','rapier'];
+let listEight = ['torch','boots of sneaking','bedroll','crossbow w/15 bolts'];
+let listNine = ['commoners clothes','leather shield','iron buckler','shortsword','lockpicks(3)'];
+let listTen = ['grappling hook','lockpicks(2)','iron-armour','potion of healing','crowwbow w/ 15 bolts'];
+let listEleven = ['bag of holding','scroll of fog','scroll of darkness','potion of healing','robes'];
+let listTwelve = ['rapier','longsword','iron shield','lockpick','throwing knives(5)'];
+let listThirteen = ['black magic robes','scroll of healing','scroll of minor illusion','scroll of protection'];
+let listFourteen = ['shortbow w/ 15 broads','bomb(10ft blast)','pike','shortsword'];
+let listFifteen = ['ironspear','pikes','ademantite shield','mithril armour','longsword','broadsword','boots of swiftness'];
+let listSixteen = ['iron spear','throwing knives(3)','shortsword','poison','longbow w/ 12 broads'];
+let listSeventeen = ['shortbow w/ 10 broads','mithri armour','pike','shortsword','boots of swiftness'];
+let listEighteen = ['commoners clothes','daggers','torches','throwing knives(6)'];
+let listNineteen = ['shortbow w/ 19 broads','poison throwing knives(3)','longsword','shortsword'];
+let listTwenty = ['torch','alcohol','potion of healing','leather armour','dagger'];
+
+let itemList = [listOne,listTwo,listThree,listFour,listFive,listSix,listSeven,listEight,listNine,listTen,listEleven,listTwelve,
+  listThirteen,listFourteen,listFifteen,listSixteen,listSeventeen,listEighteen,listNineteen,listTwenty];
